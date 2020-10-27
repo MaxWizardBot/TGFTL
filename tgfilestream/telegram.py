33ddb54b8ee34717e676ce4834700aadecbf35a5
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2020-10-27 08:35:40 +0530

    Update telegram.py

--- tgfilestream/telegram.py
+++ tgfilestream/telegram.py
@@ -16,7 +16,6 @@
 import logging
 
 from telethon import TelegramClient, events
-from telethon.sessions import StringSession
 
 from .paralleltransfer import ParallelTransferrer
 from .config import (
@@ -31,7 +30,7 @@ from .util import pack_id, get_file_name
 
 log = logging.getLogger(__name__)
 
-client = TelegramClient(StringSession(session_name), api_id, api_hash)
+client = TelegramClient(session_name, api_id, api_hash)
 transfer = ParallelTransferrer(client)
 
 
@@ -44,6 +43,6 @@ async def handle_message(evt: events.NewMessage.Event) -> None:
         await evt.reply(start_message)
         return
     url = public_url / str(pack_id(evt)) / get_file_name(evt)
-    await evt.reply(f"Link to download file: {url}")
+    await evt.reply(f"Link to download file: [{url}]({url})")
     log.info(f"Replied with link for {evt.id} to {evt.from_id} in {evt.chat_id}")
     log.debug(f"Link to {evt.id} in {evt.chat_id}: {url}")
